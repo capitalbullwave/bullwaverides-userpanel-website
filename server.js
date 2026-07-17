cac6bdf59026c4724ce2b9b115219c9c5ba98aa3
Nikesh Pandey <nikeshhzb7461@gmail.com>
2026-07-17 12:55:38 +0530

    add

--- server.js
+++ server.js
@@ -3,23 +3,16 @@ const { parse } = require("url");
 const next = require("next");
 
 const port = Number(process.env.PORT) || 3000;
-const hostname = process.env.HOSTNAME || "0.0.0.0";
+const hostname = "0.0.0.0";
 
-const app = next({
-  dev: false,
-  hostname,
-  port,
-  conf: { distDir: "out" },
-});
-
+const app = next({ dev: false, hostname, port });
 const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
     createServer((req, res) => {
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
+      handle(req, res, parse(req.url, true));
     }).listen(port, hostname, () => {
       console.log(`> Ready on http://${hostname}:${port}`);
     });
